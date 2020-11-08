c2df1303648f1d8011dd97427e23da458fe9193d
jdmedlock <jdmedlock@gmail.com>
2020-11-08 17:39:25 -0600

    Setup project repo
    Setup the project repo for the TabularData component
    
    Resolves: N/a
    See also: N/a

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,25 @@
+<script>
+	import GlobalStyle from '../components/GlobalStyle.svelte'
+	import Nav from '../components/Nav.svelte';
+
+	export let segment;
+</script>
+
+<GlobalStyle />
+
+<style>
+	main {
+		position: relative;
+		max-width: 56em;
+		background-color: white;
+		padding: 2em;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+</style>
+
+<Nav {segment}/>
+
+<main>
+	<slot></slot>
+</main>
---
+++ src/routes/index.svelte
@@ -0,0 +1,22 @@
+<script>
+	import successkid from 'images/successkid.jpg';
+</script>
+
+<style>
+	h1 {
+		@apply text-center text-5xl text-green-700 uppercase font-semibold mb-2;
+		/*
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+		*/
+	}
+
+</style>
+
+<svelte:head>
+	<title>Tabular Data Test</title>
+</svelte:head>
+
+<h1>Tabular Data Test</h1>
